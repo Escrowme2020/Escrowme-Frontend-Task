8e51290515b440d1d5974bb51c4df6d6b2ee9efb
Netrob Webby <53845292+n3trob3@users.noreply.github.com>
2020-09-02 23:49:56 -0700

    Update settings.py

--- Escrowme/Escrowme/settings.py
+++ Escrowme/Escrowme/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1rrx!o-vj#@uftv1o0#20hr9qzzo##ddym45p+c%&$@31l0q@+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
