edd5b6b873f134c372d505adc6a3a81bda88316a
Fasasi Toheeb <tohebayomide11@gmail.com>
2020-11-06 00:36:34 +0100

    Added some files

---
+++ public/js/date.js
@@ -0,0 +1,37 @@
+const textNodeList = document.querySelectorAll(".count__down__value");
+const textArrayList = Array.from(textNodeList);
+
+
+const time = setInterval(() => {
+  // Current Date
+  const now = new Date().getTime();
+  
+  // Future Date
+  const countDownDate = new Date("December 15, 2020 11:00:00").getTime();
+  
+  // Difference of the date
+  const difference = countDownDate - now;
+  
+  // Days
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  
+  // Hours
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  
+  // Minutes
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  
+  // Second
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  
+  console.log(days, hours, minutes, seconds);
+  
+  textArrayList[0].textContent = days;
+  textArrayList[1].textContent = hours;
+  textArrayList[2].textContent = minutes;
+  textArrayList[3].textContent = seconds;
+  
+  if (difference < 0) {
+    clearInterval(x);
+  };
+}, 1000);
--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,11 @@ const userDropdown = document.querySelector(".user__username");
 
 const profileSidebar = document.querySelector(".profile__sidebar");
 
+const headerMenu = document.querySelector(".header__menu");
+
+const navigation = document.querySelector(".navigation");
+const sendReceiveHeader = document.querySelector(".send__receive--header");
+
 
 
 // User drop down Handler Function
@@ -21,21 +26,80 @@ userDropdown.addEventListener("click", () => {
 
 
 // User profile Handler function
-profileSidebar.addEventListener("click", e => {
-  const el = Array.from(document.querySelectorAll(".profile__sidebar--text"));
-  
-    if(e.target.id !== "") {
-      const tar = e.target;
-      el.forEach(arr => arr.classList.remove("profile__active"));
+if(profileSidebar) {
+  profileSidebar.addEventListener("click", e => {
+    const el = Array.from(document.querySelectorAll(".profile__sidebar--text"));
     
-      if(tar.id === "0") {
-        el[0].classList.add("profile__active");
-      } else if (tar.id === "1") {
-        el[1].classList.add("profile__active");
-      } else if (tar.id === "2") {
-        el[2].classList.add("profile__active");
-      } else if (tar.id === "3") {
-        el[3].classList.add("profile__active");
+      if(e.target.id !== "") {
+        const tar = e.target;
+        el.forEach(arr => arr.classList.remove("profile__active"));
+      
+        if(tar.id === "0") {
+          el[0].classList.add("profile__active");
+        } else if (tar.id === "1") {
+          el[1].classList.add("profile__active");
+        } else if (tar.id === "2") {
+          el[2].classList.add("profile__active");
+        } else if (tar.id === "3") {
+          el[3].classList.add("profile__active");
+        }
       }
+  });
+}
+
+// Left Drop Down
+if(headerMenu) {
+  headerMenu.addEventListener("click", e => {
+    navigation.classList.toggle('active');
+		headerMenu.classList.toggle('active');
+	  if(headerMenu.classList.contains('active')){
+				headerMenu.children[0].innerHTML = '<i id="bars" class="fas fa-times"></i>';
+			} else {
+				headerMenu.children[0].innerHTML = '<i id="bars" class="fas fa-bars"></i>';
+			}
+  });
+}
+
+// Send Receive Header
+if(sendReceiveHeader) {
+  sendReceiveHeader.addEventListener("click", (e) => {
+    const classValue = e.target.className.split(" ");
+    const sendContent = document.getElementById("send__content");
+    const receiveContent = document.getElementById("receive__content");
+    const sendTab = document.querySelector(".send__tab");
+    const receiveTab = document.querySelector(".receive__tab");
+    console.log(classValue);
+    
+    classValue[1] = "";
+    // SEND TAB
+    if(classValue[0] === "send__tab") {
+      // Toggle the send__btc--active class
+      
+      receiveTab.classList.remove("send__btc--active");
+      e.target.classList.add("send__btc--active");
+      console.log("receiveTab", receiveTab.classList);
+      // Display the tab
+      if(sendContent.style.display === '' || sendContent.style.display === 'none') {
+        receiveContent.style.display = 'none';
+        sendContent.style.display = "grid";
+      }
+      console.log(e.target.className);
+      
+    // RECEIVE TAB  
+    } else if(classValue[0] === "receive__tab") {
+      // Toggle the send__btc--active class
+      
+      sendTab.classList.remove("send__btc--active");
+      e.target.classList.add("send__btc--active");
+      
+      // Display the tab
+      if(receiveContent.style.display === '' || receiveContent.style.display === 'none') {
+        sendContent.style.display = "none";
+        receiveContent.style.display = "grid";
+      }
+    
+      console.log(e.target.className);
     }
-});
+    console.log(e);
+  });
+}
